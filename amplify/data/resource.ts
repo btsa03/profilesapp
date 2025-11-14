fdd1acb88a1cf1974276adc7b9444b91fa39bc4a
Bitisha Shrestha <bitishashrestha@my.unt.edu>
2025-11-14 12:09:55 -0600

    add UserProfile data model and post-confirmation handler

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,16 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { postConfirmation } from '../auth/post-confirmation/resource';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
+  UserProfile: a
     .model({
-      content: a.string(),
+      email: a.string(),
+      profileOwner: a.string(),
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization((allow) => [
+      allow.ownerDefinedIn('profileOwner'),
+      allow.resource(postConfirmation)
+    ]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +18,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
